c1dd74c78a2af652d3fe79cfa8163416cf9e5a20
craj68168 <craj68168@gmail.com>
2026-09-02 13:04:36 +0545

    feat:github actions and eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,14 +5,24 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    linterOptions: {
+      noInlineConfig: true,
+    },
+
+    rules: {
+      "no-console": [
+        "error",
+        {
+          allow: ["warn", "error"],
+        },
+      ],
+    },
+  },
+
   // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
